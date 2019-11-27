36eb378c9c0b585e66077cdbe423c857ebc0181e
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:09:12 +0100

    Create 23. StringDemo.java

---
+++ Owoce Programowania/R02/23. StringDemo.java
@@ -0,0 +1,12 @@
+// Prosty program ilustrujący działanie obiektów typu String.
+
+public class StringDemo
+{
+   public static void main(String[] args)
+   {
+      String greeting = "Dzień dobry, ";
+      String name = "Henryku.";
+
+      System.out.println(greeting + name);
+   }
+}
