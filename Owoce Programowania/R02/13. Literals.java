48684b02ee556a60b4a9003cc0f994f093c7ebbf
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 19:20:49 +0100

    Create 13. Literals.java

---
+++ Owoce Programowania/R02/13. Literals.java
@@ -0,0 +1,14 @@
+// Ten program obejmuje literały i zmienną.
+
+public class Literals
+{
+   public static void main(String[] args)
+   {
+      int apples;
+
+      apples = 20;
+     System.out.println("Dziś sprzedaliśmy " + apples +
+                        " skrzynek jabłek.");
+   }
+}
+
