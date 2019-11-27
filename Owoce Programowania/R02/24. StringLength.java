d0baeac29732a21e0d2239d232f8da5b311c0f26
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:10:06 +0100

    Create 24. StringLength.java

---
+++ Owoce Programowania/R02/24. StringLength.java
@@ -0,0 +1,15 @@
+// Ten program ilustruje używanie metody length klasy String.
+
+public class StringLength
+{
+   public static void main(String[] args)
+   {
+      String name = "Herman";
+      int stringSize;
+
+      stringSize = name.length();
+     System.out.println(name + " składa się z " + stringSize +
+                        " znaków.");
+   }
+}
+
