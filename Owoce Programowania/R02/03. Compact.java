ca37e46cce6af28642e6233a1641f0955e198818
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 14:24:34 +0100

    Create 03. Compact.java

---
+++ Owoce Programowania/R02/03. Compact.java
@@ -0,0 +1,4 @@
+public class Compact {public static void main(String[] args){int 
+shares=220; double averagePrice=14.67; System.out.println(
+"Sprzedano "+shares+" akcji w cenie "+averagePrice+ 
+" złotego za akcję.");}}
