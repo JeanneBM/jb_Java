c74e66e8cbaa8f2ffba49621e99bb33b3b8931fd
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:51:55 +0100

    Create 30. Unruly.java

---
+++ Owoce Programowania/R02/30. Unruly.java
@@ -0,0 +1,13 @@
+// Program nieporządnie wyświetlający tekst.
+
+public class Unruly
+{
+        public static void main(String[] args)
+        {
+     System.out.print("Oto najlepiej sprzedające się produkty:");
+     System.out.print("Gry komputerowe");
+     System.out.print("Kawa");
+     System.out.println("Aspiryna");
+        }
+}
+
