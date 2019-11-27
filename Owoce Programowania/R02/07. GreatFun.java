ee9902c9786a4ad7d82dcd1ee3fb368c2224f0f9
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 14:28:03 +0100

    Create 07. GreatFun.java

---
+++ Owoce Programowania/R02/07. GreatFun.java
@@ -0,0 +1,11 @@
+// Kolejny prosty program w Javie.
+
+public class GreatFun
+{
+     public static void main(String[] args)
+     {
+          System.out.print("Programowanie to ");
+          System.out.println("świetna zabawa!");
+     }
+}
+
