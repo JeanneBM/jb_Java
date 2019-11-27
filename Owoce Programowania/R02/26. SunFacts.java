21a6338ef6d7e4b59dadd5eba42ba8e98ab13a51
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:13:24 +0100

    Create 26. SunFacts.java

---
+++ Owoce Programowania/R02/26. SunFacts.java
@@ -0,0 +1,17 @@
+// W tym programie używana jest notacja E.
+
+public class SunFacts
+{
+   public static void main(String[] args)
+   {
+      double distance, mass;
+      
+      distance = 1.495979E11;
+      mass = 1.989E30;
+     System.out.println("Odległość Ziemi od Słońca wynosi " + distance +
+                        " metra.");
+     System.out.println("Masa Słońca to " + mass +
+                        " kilograma.");
+   }
+}
+
