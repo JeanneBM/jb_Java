591a0182dfc004a99298419cced582ee50793148
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:57:14 +0100

    Create 31. Variable.java

---
+++ Owoce Programowania/R02/31. Variable.java
@@ -0,0 +1,14 @@
+// Ten program zawiera zmienną.
+
+public class Variable
+{
+   public static void main(String[] args)
+   {
+      int value;
+
+      value = 5;
+     System.out.print("Zmienna value jest równa "); 
+     System.out.println(value + ".");
+   }
+}
+
