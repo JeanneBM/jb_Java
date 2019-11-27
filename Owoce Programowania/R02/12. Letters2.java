a090166cd83dd16d2289991c38fe066216704d1c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 19:19:57 +0100

    Create 12. Letters2.java

---
+++ Owoce Programowania/R02/12. Letters2.java
@@ -0,0 +1,16 @@
+// Ten program ilustruje ścisłe powiązanie
+// między znakami a liczbami całkowitymi.
+
+public class Letters2
+{
+   public static void main(String[] args)
+   {
+      char letter;
+
+      letter = 65;
+      System.out.println(letter);
+      letter = 66;
+      System.out.println(letter);
+   }
+}
+
