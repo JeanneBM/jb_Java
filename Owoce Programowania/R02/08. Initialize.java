c814260f7184b32ab014f492edf908651f152215
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 14:29:03 +0100

    Create 08. Initialize.java

---
+++ Owoce Programowania/R02/08. Initialize.java
@@ -0,0 +1,13 @@
+// Ten  program ilustruje inicjowanie zmiennych.
+
+public class Initialize
+{
+   public static void main(String[] args)
+   {
+      int month = 2, days = 28;
+
+      System.out.println("Miesiąc nr " + month + " ma " +
+                         days + " dni.");
+   }
+}
+
