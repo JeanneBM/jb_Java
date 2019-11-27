1a33381984ee5a8479a6b57aa4efb26072105704
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:45:34 +0100

    Create 27. Tabs.java

---
+++ Owoce Programowania/R02/27. Tabs.java
@@ -0,0 +1,12 @@
+// Inny program poprawnie wyświetlający tekst.
+
+public class Tabs
+{
+   public static void main(String[] args)
+   {
+     System.out.print("Oto najlepiej sprzedające się produkty:\n");
+     System.out.print("\tGry komputerowe\n\tKawa\n");
+     System.out.println("\tAspiryna");
+   }
+}
+
