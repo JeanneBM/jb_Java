a961f0d43f2c72ace6c75e3214c643ac3b7178f3
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 19:19:13 +0100

    Create 11. Letters.java

---
+++ Owoce Programowania/R02/11. Letters.java
@@ -0,0 +1,14 @@
+// Ten ilustruje typ danych char.
+
+public class Letters
+{
+   public static void main(String[] args)
+   {
+      char letter;
+
+      letter = 'A';
+      System.out.println(letter);
+      letter = 'B';
+      System.out.println(letter);
+   }
+}
