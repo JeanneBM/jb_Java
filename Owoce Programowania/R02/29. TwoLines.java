adf510b14329cfb8363da5427131805801fb3cb2
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:50:47 +0100

    Create 29. TwoLines.java

---
+++ Owoce Programowania/R02/29. TwoLines.java
@@ -0,0 +1,11 @@
+// To następny prosty program w Javie.
+
+public class TwoLines
+{
+     public static void main(String[] args)
+     {
+      System.out.println("Programowanie to świetna zabawa!");
+      System.out.println("Nie mogę się nim nacieszyć!");
+     }
+}
+
