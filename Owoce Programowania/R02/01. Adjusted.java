aba7c8e6c79390fa01e4269c7dbdd3dc71115a47
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 14:20:01 +0100

    Update and rename Owoce Programowania/Listing.java to Owoce Programowania/R02/01. Adjusted.java

---
+++ Owoce Programowania/R02/01. Adjusted.java
@@ -0,0 +1,11 @@
+// Odpowiednio poprawiony program wyświetlający tekst.
+
+public class Adjusted
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Oto najlepiej sprzedające się produkty:\n");
+      System.out.print("Gry komputerowe\nKawa\n");
+      System.out.println("Aspiryna");
+   }
+}
