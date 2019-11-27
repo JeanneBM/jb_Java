26e5cb197bbccb219ff1b13dcd27a5912365764f
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 19:59:54 +0100

    Create 17. Readable.java

---
+++ Owoce Programowania/R02/17. Readable.java
@@ -0,0 +1,17 @@
+/** 
+   Ten przykład jest dużo bardziej czytelny niż plik Compact.java.
+*/
+
+public class Readable
+{
+   public static void main(String[] args)
+   {
+      int shares = 220;
+      double averagePrice = 14.67;
+
+     System.out.println("Sprzedano " + shares +
+                        " akcji w cenie " +
+                        averagePrice + " złotego za akcję.");
+   }
+}
+
