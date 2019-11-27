cf27ddba5adf21ef6a52fadaac80d6a10788dbb4
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:47:08 +0100

    Create 28. TrueFalse.java

---
+++ Owoce Programowania/R02/28. TrueFalse.java
@@ -0,0 +1,15 @@
+// Program ilustrujący zmienne logiczne.
+
+public class TrueFalse
+{
+   public static void main(String[] args)
+   {
+      boolean bool;
+
+      bool = true;
+      System.out.println(bool);
+      bool = false;
+      System.out.println(bool);
+   }
+}
+
