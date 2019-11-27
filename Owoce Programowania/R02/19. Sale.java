e2d96e4d82b5464ee6674cf86d30a916a593f7e0
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-27 20:06:00 +0100

    Create 19. Sale.java

---
+++ Owoce Programowania/R02/19. Sale.java
@@ -0,0 +1,18 @@
+// W tym programie pokazano, jak używać typu danych double.
+
+public class Sale
+{
+   public static void main(String[] args)
+   {
+      double price, tax, total;
+
+      price = 29.75;
+      tax = 1.76;
+      total = 31.51;
+     System.out.println("Cena produktu " +
+                        "wynosi " + price + ".");
+     System.out.println("Podatek wynosi " + tax + ".");
+     System.out.println("Łączna kwota wynosi " + total + ".");
+   }
+}
+
