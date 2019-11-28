6acd2a134905935b981a3423e165911b39a237c8
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-11-28 23:43:16 +0100

    Create 25. WhileLoop.java

---
+++ Owoce Programowania/R04/25. WhileLoop.java
@@ -0,0 +1,19 @@
+/**
+   Ten program ilustruje działanie pętli while.
+*/
+
+public class WhileLoop
+{
+   public static void main(String [] args)
+   {
+      int number = 1;
+
+      while (number <= 5)
+      {
+         System.out.println("Witaj");
+         number++;
+      }
+
+      System.out.println("To już wszystko!");
+   }
+}
