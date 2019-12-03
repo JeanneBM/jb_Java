ef69faea26c9fc1129e4898c8f1b528f195fa437
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-03 23:46:16 +0100

    Create 01. ArrayDemo1.java

---
+++ Owoce Programowania/R07/01. ArrayDemo1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;   // Needed for Scanner class
+
+/**
+   Ten program pokazuje, jak zapisywać
+   i wyświetlać elementy tablicy.
+*/
+
+public class ArrayDemo1
+{
+   public static void main(String[] args)
+   {
+      final int EMPLOYEES = 3;           // Liczba pracowników.
+      int[] hours = new int[EMPLOYEES];  // Tablica z przepracowanymi godzinami.
+
+      // Tworzenie obiektu typu Scanner do pobierania danych z klawiatury.
+      Scanner keyboard = new Scanner(System.in);
+      
+      System.out.println("Podaj liczbę godzin przepracowanych przez " +
+                         EMPLOYEES + " pracowników.");
+
+      // Pobieranie liczby godzin przepracowanych przez pracownika numer 1.
+      System.out.print("Pracownik numer 1: ");
+      hours[0] = keyboard.nextInt();
+
+      // Pobieranie liczby godzin przepracowanych przez pracownika numer 2.
+      System.out.print("Pracownik numer 2: ");
+      hours[1] = keyboard.nextInt();
+
+      // Pobieranie liczby godzin przepracowanych przez pracownika numer 3.
+      System.out.print("Pracownik numer 3: ");
+      hours[2] = keyboard.nextInt();
+
+      // Wyświetlanie podanych wartości.
+      System.out.println("Wpisana liczba godzin to:");
+      System.out.println(hours[0]);
+      System.out.println(hours[1]);
+      System.out.println(hours[2]);
+   }
+}     
