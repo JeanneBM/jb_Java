d682703fe06ef99360dc728abe49dadce5a1cad1
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:54:05 +0100

    Create 10. SubClass1.java

---
+++ Owoce Programowania/R10/10. SubClass1.java
@@ -0,0 +1,12 @@
+public class SubClass1 extends SuperClass1
+{
+   /**
+      Konstruktor.
+   */
+
+   public SubClass1()
+   {
+      System.out.println("To konstruktor " +
+                        "klasy pochodnej.");
+   }
+}
