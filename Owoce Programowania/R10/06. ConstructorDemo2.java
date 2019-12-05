3f02e12482eda7b8e997530f74011217a3048d78
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 18:31:32 +0100

    Create 06. ConstructorDemo2.java

---
+++ Owoce Programowania/R10/06. ConstructorDemo2.java
@@ -0,0 +1,12 @@
+/**
+   Ten program pokazuje, jak wywoływać konstruktor
+   klasy bazowej za pomocą słowa kluczowego super.
+*/
+
+public class ConstructorDemo2
+{
+   public static void main(String[] args)
+   {
+      SubClass2 obj = new SubClass2();
+   }
+}
