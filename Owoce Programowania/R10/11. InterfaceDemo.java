cb75d1fd7b892f3255f79b2e8dff0f17a9108616
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:40:10 +0100

    Create 11. InterfaceDemo.java

---
+++ Owoce Programowania/R10/11. InterfaceDemo.java
@@ -0,0 +1,17 @@
+/**
+   Ten program ilustruje działanie klasy z implementacją
+   interfejsu Displayable.
+*/
+
+public class InterfaceDemo
+{
+   public static void main(String[] args)
+   {
+      // Tworzenie instancji klasy Person.
+      Person p = new Person("Antoni");
+
+      // Wywołanie metody display obiektu.
+      p.display();
+   }
+}
+
