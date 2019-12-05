cadca80bfda7b0f705d11a356eeee4eef678b663
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:55:33 +0100

    Create 10. SuperClass1.java

---
+++ Owoce Programowania/R10/10. SuperClass1.java
@@ -0,0 +1,12 @@
+public class SuperClass1
+{
+   /**
+      Konstruktor.
+   */
+
+   public SuperClass1()
+   {
+      System.out.println("To konstruktor " +
+                         "klasy bazowej.");
+   }
+}
