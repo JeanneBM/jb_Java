1982fa37cebfdd20ea14b4442d218149557f201b
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:56:04 +0100

    Create 10. SuperClass2.java

---
+++ Owoce Programowania/R10/10. SuperClass2.java
@@ -0,0 +1,23 @@
+public class SuperClass2
+{
+   /**
+      Konstruktor numer 1.
+   */
+
+   public SuperClass2()
+   {
+      System.out.println("To konstruktor bezargumentowy" +
+                         "klasy bazowej.");
+   }
+
+   /**
+      Konstruktor numer 2.
+   */
+
+   public SuperClass2(int arg)
+   {
+      System.out.println("Do konstruktora " +
+                        "klasy bazowej przekazano " +
+                        "następujący argument: " + arg);
+   }
+}
