bbe7e51433bac4946af79cd201f67e7051e99aef
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:36:39 +0100

    Create 09. Relatable.java

---
+++ Owoce Programowania/R10/09. Relatable.java
@@ -0,0 +1,10 @@
+/**
+   Interfejs Relatable.
+*/
+
+public interface Relatable
+{
+   boolean equals(GradedActivity g);
+   boolean isGreater(GradedActivity g);
+   boolean isLess(GradedActivity g);
+}
