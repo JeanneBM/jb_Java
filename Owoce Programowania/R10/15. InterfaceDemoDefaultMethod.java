2e5ee13a1ed185b93e4f9cf37965e81a71d2c035
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 23:58:27 +0100

    Create 15. InterfaceDemoDefaultMethod.java

---
+++ Owoce Programowania/R10/15. InterfaceDemoDefaultMethod.java
@@ -0,0 +1,16 @@
+/**
+   Ten program ilustruje klasę z implementacją
+   interfejsu Displayable (zawierającego metodę domyślną).
+*/
+
+public class InterfaceDemoDefaultMethod
+{
+   public static void main(String[] args)
+   {
+      // Tworzenie obiektu typu Person.
+     Person p = new Person("Antoni");
+
+     // Wywołanie metody display obiektu.
+      p.display();
+   }
+}
