fa80962eff4168ddbfe9b7ac8a7bfd95cc631298
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-05 18:30:31 +0100

    Create 05. ConstructorDemo1.java

---
+++ Owoce Programowania/R10/05. ConstructorDemo1.java
@@ -0,0 +1,12 @@
+/**
+   Ten program ilustruje kolejność wywoływania
+   konstruktorów klas bazowej i pochodnej.
+*/
+
+public class ConstructorDemo1
+{
+   public static void main(String[] args)
+   {
+      SubClass1 obj = new SubClass1();
+   }
+}
