92b36208710852b604158f2ed374813c9c68d4fc
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-14 23:51:36 +0100

    Create 06. DropShadowDemo2.java

---
+++ Owoce Programowania/R14/06. DropShadowDemo2.java
@@ -0,0 +1,43 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.effect.DropShadow;
+
+public class DropShadowDemo2 extends Application
+{
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+   
+   @Override
+   public void start(Stage primaryStage)
+   {
+      // Stałe określające wielkość sceny.
+      final double SCENE_WIDTH = 200.0;
+      final double SCENE_HEIGHT = 200.0;
+      
+      // Stałe do tworzenia obiektu typu Circle.
+      final double X = 100.0, Y = 100.0, RADIUS = 50.0;
+            
+      // Tworzenie obiektu typu DropShadow.
+      DropShadow dropShadow = new DropShadow();
+      dropShadow.setOffsetX(10);
+      dropShadow.setOffsetY(10);
+      
+      // Tworzenie obiektu typu Circle.
+      Circle myCircle = new Circle(X, Y, RADIUS, Color.RED);
+      myCircle.setEffect(dropShadow);
+       
+      // Dodawanie obiektu typu Circle do obiektu typu Pane.
+      Pane pane = new Pane(myCircle);
+      
+      // Tworzenie sceny i jej wyświetlanie.
+      Scene scene = new Scene(pane, SCENE_WIDTH, SCENE_HEIGHT);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+   }
+}
