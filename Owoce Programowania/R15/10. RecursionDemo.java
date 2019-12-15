189aba0d479f1fc1792366bbb6725433374a293b
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-15 23:35:01 +0100

    Create 10. RecursionDemo.java

---
+++ Owoce Programowania/R15/10. RecursionDemo.java
@@ -0,0 +1,11 @@
+/**
+   Ta klasa ilustruje działanie metody Recursive.message.
+*/
+
+public class RecursionDemo
+{
+   public static void main(String[] args)
+   {
+      Recursive.message(5);
+   }
+}
