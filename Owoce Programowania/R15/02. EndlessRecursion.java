c24dc7deaa6d45828b07e8080b68d004624435de
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-15 23:29:58 +0100

    Create 02. EndlessRecursion.java

---
+++ Owoce Programowania/R15/02. EndlessRecursion.java
@@ -0,0 +1,12 @@
+/**
+   Ta klasa zawiera metodę rekurencyjną.
+*/
+
+public class EndlessRecursion
+{
+   public static void message()
+   {
+      System.out.println("To metoda rekurencyjna.");
+      message();
+   }
+}
