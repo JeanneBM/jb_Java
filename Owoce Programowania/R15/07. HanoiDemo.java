9d775512d1c9b5a252d3d0ce9a011cd92c6fe079
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2019-12-15 23:32:56 +0100

    Create 07. HanoiDemo.java

---
+++ Owoce Programowania/R15/07. HanoiDemo.java
@@ -0,0 +1,13 @@
+/**
+   Ta klasa ilustruje działanie klasy Hanoi, 
+   wyświetlającej kroki potrzebne do 
+   rozwiązania łamigłówki Wieże Hanoi.
+*/
+
+public class HanoiDemo
+{
+   static public void main(String[] args)
+   {
+      Hanoi towersOfHanoi = new Hanoi(3);
+   }
+}
