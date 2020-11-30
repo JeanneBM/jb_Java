dec226c0eb6a0d382b646f07416bddd485e006a0
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-11-30 18:45:34 +0100

    Create 01. Dwuelementowa tablica liczb.java

---
+++ Samouczek/02. Tablice/01. Dwuelementowa tablica liczb.java
@@ -0,0 +1,14 @@
+//Napisz metodę pobierającą dwuelementową tablicę liczb i zwracający ich sumę.
+
+
+public class Exercise1 {
+    public int sum(int[] numbers) {
+        return numbers[0] + numbers[1];
+    }
+
+    public static void main(String[] args) {
+        Exercise1 exercise = new Exercise1();
+        int[] numbers = {3, 4};
+        System.out.println(exercise.sum(numbers));
+    }
+}
