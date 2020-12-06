acdf35911db53d90717feb386cc25b6e25a097b9
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-12-06 19:08:41 +0100

    Create 03. BankAccount.java

---
+++ Samouczek/13. Modyfikatory dostępu/01. Symulacja banku/03. BankAccount.java
@@ -0,0 +1,32 @@
+class BankAccount implements Account {
+
+    int balance;
+
+    BankAccount() {
+        this(0);
+    }
+
+    BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public void deposit(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("ammount must be > 0");
+        }
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("ammount must be > 0");
+        }
+        balance -= amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
